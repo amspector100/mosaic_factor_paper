66199855d2090e9e254626f45bf781017fa18559
amspector100 <amspector100@gmail.com>
2025-09-02 00:00:11 -0700

    updated for cluster

--- sims/robustness_sims_array.sh
+++ sims/robustness_sims_array.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/bash
 #SBATCH --ntasks 1
-#SBATCH --array=1-2000:10
+#SBATCH --array=1-4000:20
 #SBATCH --job-name=mpt_sim
 #SBATCH --output=../log/robustness_sim_%A_%a.out
 #SBATCH --error=../log/robustness_sim_%A_%a.err
 #SBATCH --time=23:59:00
-#SBATCH -p candes 
+#SBATCH -p candes,stat,hns,normal
 #SBATCH -c 1
 #SBATCH --mem=4GB
 
-NREPS=10 # make sure this is the same as the job array step size
+NREPS=20 # make sure this the same as the job array step size
 NPROC=1
 
 MAIN_ARGS="
         --sampling_method [ar1,garch,garch_ar1,mvn_arch]
         --industry [EGY,FIN,HLC]
-        --n [100,200,300,400,500]
+        --n [300]
         --reps $NREPS
         --num_processes $NPROC
         --seed_start $SLURM_ARRAY_TASK_ID
@@ -24,6 +24,5 @@ MAIN_ARGS="
 "
 
 
-module load python/3.9
-module load py-numpy/1.20.3_py39
-python3.9 robustness_sims.py $MAIN_ARGS
+source /home/users/aspector/mosaic/setup_env.sh
+python robustness_sims.py $MAIN_ARGS
